635d75b88894174045073ee471f2743ba7683f6e
SimonAllenOliver <simonallenoliver@gmail.com>
2024-03-12 21:47:31 -0500

    crud delish

---
+++ Models/DishModel.cs
@@ -0,0 +1,11 @@
+
+public class Dish
+{
+    public int DishId { get; set; }
+    public string Name { get; set; }
+    public string Chef { get; set; }
+    public int Tastiness { get; set; }
+    public int Calories { get; set; }
+    public string Descrption
+
+}
